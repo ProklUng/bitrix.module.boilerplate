585119f358f7e08eb6115c1d6634062eae82fe29
Fedor Kavkazcky <funtik21975@gmail.com>
2021-04-18 10:00:29 +0300

    Init repo.

---
+++ src/Exception/MainModuleManagerException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ProklUng\Module\Boilerplate\Exception;
+
+use RuntimeException;
+
+/**
+ * Class MainModuleManagerException
+ * @package ProklUng\Module\Boilerplate\Exception
+ *
+ * @since 15.04.2021
+ */
+class MainModuleManagerException extends RuntimeException
+{
+
+}
